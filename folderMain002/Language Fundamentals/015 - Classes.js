d2e1c948e45dda0e9b156230d2c0e3f5b3adc116
lukaofirst <lucasmacedo123@gmail.com>
2021-10-27 21:17:28 -0300

    adding new lessons to folderMain002

--- folderMain002/Language Fundamentals/015 - Classes.js
+++ folderMain002/Language Fundamentals/015 - Classes.js
@@ -65,3 +65,73 @@ person1.showArr();
 const product = {
     _internalId: 'abc1'
 }; */
+
+/* --- instanceof --- */
+// If you have a doubt if a instaciated object was created from a specific class,
+// use the 'instanceof' operator
+
+// Example
+console.log(person1 instanceof Person);
+
+/* --- Object Descriptors --- */
+// They're method that belongs to Object(), they can be useful, to check or define object properties
+const objectLiteral = {
+    teamName: 'GODSENT',
+    players: ['TACO', 'felps', 'dumau', 'b4rt1n', 'latto'],
+    ranking: 23,
+    location: 'Brazil',
+};
+
+// console.log(Object.getOwnPropertyDescriptors(objectLiteral));
+
+// configurable
+// true => if the type of this property descriptor may be changed and if the property may be delete from the object
+Object.defineProperty(objectLiteral, 'teamName', {
+    configurable: true,
+    /* enumerable: false, */
+    /* value: objectLiteral.teamName, */
+    /* writable: false */
+});
+
+delete objectLiteral.teamName;
+
+console.log(objectLiteral);
+
+// enumerable
+// true => if and only if this property shows up during enumeration of the properties
+Object.defineProperty(objectLiteral, 'players', {
+    /* configurable: false, */
+    enumerable: false,
+    /* value: objectLiteral.players, */
+    /* writable: false, */
+});
+
+for (let key in objectLiteral) {
+    console.log([key]);
+}
+
+console.log(objectLiteral);
+
+// value
+// The value associated with the property. Defaults to undefined
+Object.defineProperty(objectLiteral, 'ranking', {
+    /* configurable: false, */
+    /* enumerable: false */
+    value: 10,
+    /* writable: false */
+});
+
+console.log(objectLiteral);
+
+// writable
+// true => if the value associated with the property may be changed with an assignment operator
+Object.defineProperty(objectLiteral, 'location', {
+    /* configurable: false, */
+    /* enumerable: false */
+    /* value: objectLiteral.location */
+    writable: true,
+});
+
+objectLiteral.location = 'Sweden';
+
+console.log(objectLiteral);
